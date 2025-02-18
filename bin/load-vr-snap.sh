425f69b3325cd4aeb6f65b30a17d0730b4fce6b6
Joyce Krawiec <jkrawiec31@proton.me>
2025-02-17 19:22:30 -0500

    Sort the VR list

--- bin/load-vr-snap.sh
+++ bin/load-vr-snap.sh
@@ -7,7 +7,7 @@ zerofile="${zero#zerodir/}"
 Main(){
 	local bucketdir="${BUCKET_DIR:-${zerodir}/../../buckets}"
 	local files=()
-	readarray -t files < <(find "${bucketdir}/dl.ncsbe.gov/data/Snapshots" -name VR_Snapshot\*.zip)
+	readarray -t files < <(find "${bucketdir}/dl.ncsbe.gov/data/Snapshots" -name VR_Snapshot\*.zip | sort)
 	: "${L_DB:=${zerofile%.*}.sqlite3}"
 	rm -f "${L_DB}.tmp" || :
 	loadit "${L_DB}.tmp" "${files[@]}"
