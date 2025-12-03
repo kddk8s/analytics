64189399db61b2db798802679f7977d20939a639
Joyce Krawiec <jkrawiec31@proton.me>
2025-12-03 11:59:10 -0500

    Handle 2025 municipal data load

--- bin/load-2025-q1-muni.sh
+++ bin/load-2025-q1-muni.sh
@@ -3,6 +3,7 @@ set -e -u -o pipefail
 set -x
 zero=$(readlink -f "$0")
 zerodir="${zero%/*}"
+zerofile="${zero#zerodir/}"
 Main(){
 	local bucketdir="${BUCKET_DIR:-${zerodir}/../../buckets}"
 	local files=(
@@ -13,7 +14,13 @@ Main(){
 	# Want the last presidential (Which is also congressional)
 	"${bucketdir}/dl.ncsbe.gov/data/Snapshots/VR_Snapshot_20241105.zip"
 	# Want part of the vipVeed from same (For streets)
-	${bucketdir}/dl.ncsbe.gov/data/vipFeed/vipFeed-37-2024-11-05.zip
+	"${bucketdir}/dl.ncsbe.gov/data/vipFeed/vipFeed-37-2024-11-05.zip"
 	"${bucketdir}/dl.ncsbe.gov/Elections"/*/"Candidate Filing"/*.csv
 	)
+	: "${MUNI_DB:=${zerofile%.*}.sqlite3}"
+	rm -f "${MUNI_DB}.tmp" || :
+	loadit "${MUNI_DB}.tmp" "${files[@]}"
+	mv -v "${MUNI_DB}.tmp" "${MUNI_DB}"
+	exit 0
 }
+Main "$@"
