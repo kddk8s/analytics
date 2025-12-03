6088ccd1be23cca6e7c5c24a168e81f7c92976e8
Joyce Krawiec <jkrawiec31@proton.me>
2025-12-03 11:59:10 -0500

    vr_snap loader

---
+++ bin/load-vr-snap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e -u -o pipefail
+set -x
+zero=$(readlink -f "$0")
+zerodir="${zero%/*}"
+zerofile="${zero#zerodir/}"
+Main(){
+	local bucketdir="${BUCKET_DIR:-${zerodir}/../../buckets}"
+	local files=()
+	readarray -t files < <(find "${bucketdir}/dl.ncsbe.gov/data/Snapshots" -name VR_Snapshot\*.zip)
+	: "${L_DB:=${zerofile%.*}.sqlite3}"
+	rm -f "${L_DB}.tmp" || :
+	loadit "${L_DB}.tmp" "${files[@]}"
+	mv -v "${L_DB}.tmp" "${L_DB}"
+	exit 0
+}
+Main "$@"
